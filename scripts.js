f9a0eef2477848a021ae04ce54fd0d621c479359
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-09-11 20:30:20 -0300

    :heavy_plus_sign: Add @squidit/css dependency

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
+function readTextFile(file, callback) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType("application/json");
+  rawFile.open("GET", file, true);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4 && rawFile.status == "200") {
+      callback(rawFile.responseText);
+    }
+  }
+  rawFile.send(null);
+}
 
+
+document.addEventListener("DOMContentLoaded", async function () {
+  readTextFile('./assets/data.json', function (text) {
+    var data = JSON.parse(text);
+    console.log(data);
+  });
 });
